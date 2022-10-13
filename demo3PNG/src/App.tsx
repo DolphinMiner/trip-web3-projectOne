3ffefe363dac4df46c68d53a5550bf06842eddb8
Sean Wang <sean.wang@skyscanner.net>
2022-10-13 19:24:48 +0800

    chore: add onDownload

--- demo3PNG/src/App.tsx
+++ demo3PNG/src/App.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import styles from "./App.module.css";
 import { toPng } from "html-to-image";
-import html2canvas from "html2canvas";
 import Avatar from "./components/Avatar";
 import pngSource from "./png";
 import { Attributes, LayerName } from "./types";
@@ -10,6 +9,7 @@ import { Action, initialState, reducer } from "./reducer/attributes";
 import { shuffle } from "./utils";
 
 const App = () => {
+  const avatarRef = useRef<HTMLDivElement | null>(null);
   const [attributes, dispatch] = useReducer(reducer, initialState);
 
   const onShuffle = () => {
@@ -20,17 +20,36 @@ const App = () => {
     });
   };
 
+  const onDownload = () => {
+    if (!window || !avatarRef.current) {
+      return;
+    }
+    toPng(avatarRef.current)
+      .then(blobUrl => {
+        const anchor = window.document.createElement('a');
+        anchor.style.display = 'none';
+        anchor.href = blobUrl;
+        anchor.download = "avatar.png";
+        document.body.appendChild(anchor);
+        anchor.click();
+      })
+      .catch(err => {
+        console.log('Failed to download', err);
+      });
+  }
+
   useEffect(() => {
     onShuffle();
   }, []);
 
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
+      <div className={styles.container} ref={avatarRef}>
         <Avatar attributes={attributes} />
       </div>
       <div className={styles.btnContainer}>
-        <div className={styles.randomBtn} onClick={onShuffle} />
+        <button onClick={onShuffle}>Shuffle</button>
+        <button onClick={onDownload}>Download</button>
       </div>
     </main>
   );
