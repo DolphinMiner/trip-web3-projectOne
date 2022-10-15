f78d1a34ab38b11465b785faffa353df2424666b
Sean Wang <sean.wang@skyscanner.net>
2022-10-15 19:47:22 +0800

    chore: move download to utils

--- demo3PNG/src/App.tsx
+++ demo3PNG/src/App.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
 import React, { useEffect, useReducer, useRef, useState } from "react";
 import styles from "./App.module.css";
-import { toBlob, toPng } from "html-to-image";
 import Avatar from "./components/Avatar";
 import { Attributes, LayerName } from "./types";
-import { shuffle } from "./utils";
-import { saveAs } from "file-saver";
-import JSZip from "jszip";
+import { download, shuffle } from "./utils";
 import configs from "./configs";
 import useAvatar from "./hooks/useAvatar";
 
@@ -21,32 +18,10 @@ const App = () => {
   };
 
   const onDownload = () => {
-    if (!window || !avatarRef.current) {
+    if (!avatarRef.current) {
       return;
     }
-    toBlob(avatarRef.current)
-      .then((pngBlob) => {
-        if (!pngBlob) throw Error("Failed to toBlob");
-
-        const jsonBlob = new Blob([JSON.stringify(attributes)], {
-          type: "text/plain;charset=utf-8",
-        });
-
-        const zip = new JSZip();
-        zip.file("/png/avatar.png", pngBlob);
-        zip.file("/json/metadata.json", jsonBlob);
-        zip
-          .generateAsync({ type: "blob" })
-          .then((blob) => {
-            saveAs(blob, "avatar.zip");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log("Failed to download", err);
-      });
+    download(attributes, avatarRef.current);
   };
 
   useEffect(() => {
--- demo3PNG/src/utils/index.ts
+++ demo3PNG/src/utils/index.ts
@@ -1,3 +1,6 @@
+import { saveAs } from "file-saver";
+import { toBlob } from "html-to-image";
+import JSZip from "jszip";
 import configs from "../configs";
 import { Attributes } from "../types";
 
@@ -10,4 +13,24 @@ export const shuffle = (): Attributes => {
       [cur]: collection[Math.floor(Math.random() * collection.length)],
     };
   }, {});
+};
+
+export const download = async (attributes: Attributes, ele: HTMLDivElement) => {
+  try {
+    const pngBlob = await toBlob(ele);
+    const jsonBlob = new Blob([JSON.stringify(attributes)], {
+      type: "text/plain;charset=utf-8",
+    });
+    if (!pngBlob || !jsonBlob) {
+      throw new Error("Failed to get blob data", { pngBlob, jsonBlob });
+    }
+    const zip = new JSZip();
+    zip.file("/png/avatar.png", pngBlob);
+    zip.file("/json/metadata.json", jsonBlob);
+    await zip.generateAsync({ type: "blob" }).then((blob) => {
+      saveAs(blob, "avatar.zip");
+    });
+  } catch (e) {
+    console.log("Failed to download", e);
+  }
 };
