cfd123f81ac3c595460d1fda0ea16b69db28fdb2
Harry Liu <harrybaa@gmail.com>
2022-10-15 22:04:44 +0800

    feat: 修改demo3首页为tab切换页面

--- demo3PNG/src/App.tsx
+++ demo3PNG/src/App.tsx
@@ -8,10 +8,16 @@ import { Attributes, LayerName } from "./types";
 import { Action, initialState, reducer } from "./reducer/attributes";
 import { shuffle } from "./utils";
 import { saveAs } from "file-saver";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import ShufflePanel from "./components/ShufflePanel";
 
 const App = () => {
   const avatarRef = useRef<HTMLDivElement | null>(null);
   const [attributes, dispatch] = useReducer(reducer, initialState);
+  const [selectedPanel, setSelectedPanel] = useState<"patch" | "shuffle">(
+    "patch"
+  );
 
   const onShuffle = () => {
     const payload = shuffle();
@@ -26,33 +32,69 @@ const App = () => {
       return;
     }
     toPng(avatarRef.current)
-      .then(pngBlob => {
+      .then((pngBlob) => {
         // image
         saveAs(pngBlob, "avatar.png");
         // json
-        const jsonBlob = new Blob(
-          [JSON.stringify(attributes)],
-          {type: "text/plain;charset=utf-8"}
-        );
-        saveAs(jsonBlob, 'metadata.json');
+        const jsonBlob = new Blob([JSON.stringify(attributes)], {
+          type: "text/plain;charset=utf-8",
+        });
+        saveAs(jsonBlob, "metadata.json");
       })
-      .catch(err => {
-        console.log('Failed to download', err);
+      .catch((err) => {
+        console.log("Failed to download", err);
       });
-  }
+  };
 
   useEffect(() => {
     onShuffle();
   }, []);
 
+  const renderPatchPanel = () => {
+    return (
+      <>
+        <div className={styles.container} ref={avatarRef}>
+          <Avatar attributes={attributes} />
+        </div>
+        <div className={styles.btnContainer}>
+          <Button variant="contained" onClick={onShuffle}>
+            Shuffle
+          </Button>
+          <Button variant="contained" onClick={onDownload}>
+            Download
+          </Button>
+        </div>
+      </>
+    );
+  };
+
+  const renderShufflePanel = () => {
+    return <ShufflePanel />;
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.container} ref={avatarRef}>
-        <Avatar attributes={attributes} />
+      <div className={styles.headerButtonGroup}>
+        <ButtonGroup variant="contained">
+          <Button
+            disabled={selectedPanel === "patch"}
+            onClick={() => setSelectedPanel("patch")}
+            variant={selectedPanel === "patch" ? "outlined" : "contained"}
+          >
+            手动组合
+          </Button>
+          <Button
+            disabled={selectedPanel === "shuffle"}
+            onClick={() => setSelectedPanel("shuffle")}
+            variant={selectedPanel === "shuffle" ? "outlined" : "contained"}
+          >
+            批量生成
+          </Button>
+        </ButtonGroup>
       </div>
-      <div className={styles.btnContainer}>
-        <button onClick={onShuffle}>Shuffle</button>
-        <button onClick={onDownload}>Download</button>
+
+      <div className={styles.panelBox}>
+        {selectedPanel === "patch" ? renderPatchPanel() : renderShufflePanel()}
       </div>
     </main>
   );
