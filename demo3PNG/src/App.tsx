63c09c20f3fc9bf1194f23c7162db68dc2325472
Harry Liu <harrybaa@gmail.com>
2022-10-15 22:17:30 +0800

    feat: add tab switcher for demo3 page

--- demo3PNG/src/App.tsx
+++ demo3PNG/src/App.tsx
@@ -1,15 +1,19 @@
-import Image from "next/image";
 import React, { useEffect, useReducer, useRef, useState } from "react";
 import styles from "./App.module.css";
 import Avatar from "./components/Avatar";
-import { Attributes, LayerName } from "./types";
 import { download, shuffle } from "./utils";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import ShufflePanel from "./components/ShufflePanel";
 import configs from "./configs";
 import useAvatar from "./hooks/useAvatar";
 
 const App = () => {
   const avatarRef = useRef<HTMLDivElement | null>(null);
   const { attributes, setAttributes } = useAvatar();
+  const [selectedPanel, setSelectedPanel] = useState<"patch" | "shuffle">(
+    "patch"
+  );
 
   const onShuffle = () => {
     const payload = shuffle();
@@ -28,18 +32,55 @@ const App = () => {
     onShuffle();
   }, []);
 
+  const renderPatchPanel = () => {
+    return (
+      <>
+        <div className={styles.container} ref={avatarRef}>
+          <Avatar
+            source={configs.pngSource}
+            layers={configs.layers}
+            attributes={attributes}
+          />
+        </div>
+        <div className={styles.btnContainer}>
+          <Button variant="contained" onClick={onShuffle}>
+            Shuffle
+          </Button>
+          <Button variant="contained" onClick={onDownload}>
+            Download
+          </Button>
+        </div>
+      </>
+    );
+  };
+
+  const renderShufflePanel = () => {
+    return <ShufflePanel />;
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.container} ref={avatarRef}>
-        <Avatar
-          source={configs.pngSource}
-          layers={configs.layers}
-          attributes={attributes}
-        />
+      <div className={styles.headerButtonGroup}>
+        <ButtonGroup variant="contained">
+          <Button
+            disabled={selectedPanel === "patch"}
+            onClick={() => setSelectedPanel("patch")}
+            variant={selectedPanel === "patch" ? "outlined" : "contained"}
+          >
+            手动组合
+          </Button>
+          <Button
+            disabled={selectedPanel === "shuffle"}
+            onClick={() => setSelectedPanel("shuffle")}
+            variant={selectedPanel === "shuffle" ? "outlined" : "contained"}
+          >
+            批量生成
+          </Button>
+        </ButtonGroup>
       </div>
-      <div className={styles.btnContainer}>
-        <button onClick={onShuffle}>Shuffle</button>
-        <button onClick={onDownload}>Download</button>
+
+      <div className={styles.panelBox}>
+        {selectedPanel === "patch" ? renderPatchPanel() : renderShufflePanel()}
       </div>
     </main>
   );
