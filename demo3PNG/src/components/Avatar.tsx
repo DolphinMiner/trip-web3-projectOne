ac6739fad2c15b7ab82caa4db297ecc504d330ca
Xiaobo2020 <1971822513@qq.com>
2022-10-16 22:01:02 +0800

    Demo3: Use classnames (#14)
    * fix: unclosed style
    
    * chore: add classnames
    
    * chore: use classnames for Avatar
    
    * chore: add props.className for Avatar
    
    * chore: remove small preset in Avatar
    
    Co-authored-by: Sean Wang <sean.wang@skyscanner.net>

--- demo3PNG/src/components/Avatar.tsx
+++ demo3PNG/src/components/Avatar.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { LayerName } from "../types";
 import Layer from "./Layer";
 import styles from "./Avatar.module.css";
+import classnames from "classnames";
 
 type AvatarProps = {
   attributes: Record<LayerName, string>;
   layers: LayerName[];
   source: Record<string, Record<string, any>>;
-  isSmall?: boolean;
+  className?: string;
 };
 
-const Avatar = ({ attributes, layers, source, isSmall }: AvatarProps) => {
+const Avatar = ({ attributes, layers, source, className }: AvatarProps) => {
   return (
-    <div className={[styles.container, isSmall ? styles.smallContainer : null]}>
+    <div className={classnames([
+      styles.container,
+      className ? className : null,
+    ])}>
       {layers.map((layer) => {
         return attributes[layer] &&
           source[layer] &&
