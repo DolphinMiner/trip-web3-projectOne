066cf8e01d8c8ec6bd15ed46e7ac0ae8f3441411
Xiaobo2020 <1971822513@qq.com>
2022-10-18 09:11:08 +0800

    Demo3 refactor layout (#17)
    * chore: formate style for patch panel
    
    * chore: extract PatchPanel
    
    * chore: update style
    
    * chore: move PatchPanel to components
    
    * chore: left and right container in PatchPanel
    
    * chore: side style
    
    * chore: update side style
    
    * chore: add border
    
    Co-authored-by: Sean Wang <sean.wang@skyscanner.net>

---
+++ demo3PNG/src/components/PatchPanel.tsx
@@ -0,0 +1,103 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from "@mui/material";
+import React, { useEffect, useRef } from "react";
+import Avatar from "./Avatar";
+import configs from "../configs";
+import useAvatar from "../hooks/useAvatar";
+import { download, shuffle } from "../utils";
+import styles from "./PatchPanel.module.css";
+
+const PatchPanel = () => {
+  const avatarRef = useRef<HTMLDivElement | null>(null);
+  const { attributes, setAttributes } = useAvatar();
+
+  const onShuffle = () => {
+    const payload = shuffle();
+    setAttributes(payload);
+    console.log(payload);
+  };
+
+  const onDownload = () => {
+    if (!avatarRef.current) {
+      return;
+    }
+    download(attributes, avatarRef.current);
+  };
+
+  useEffect(() => {
+    onShuffle();
+  }, []);
+
+  return (
+    <Grid className={styles.patchPanelContainer} container spacing={0}>
+      <Grid item xs={"auto"} className={styles.leftContainer}>
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            position: "relative",
+            overflow: "auto",
+            maxHeight: 720,
+          }}
+        >
+          {configs.layers.map((layer) => (
+            <li key={`section-${layer}`}>
+              <ul>
+                <ListSubheader>{`${layer.toUpperCase()}`}</ListSubheader>
+                {Object.keys(configs.attributes[layer]).map((layerStyle) => (
+                  <ListItem key={`item-${layer}-${layerStyle}`}>
+                    <ListItemButton
+                      role={undefined}
+                      onClick={() => setAttributes({ [layer]: layerStyle })}
+                      dense
+                    >
+                      <ListItemIcon style={{ minWidth: 30 }}>
+                        <Checkbox
+                          edge="start"
+                          checked={layerStyle === attributes[layer]}
+                          tabIndex={-1}
+                          disableRipple
+                        />
+                      </ListItemIcon>
+                      <ListItemText primary={`${layerStyle}`} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </List>
+      </Grid>
+      <Grid item xs className={styles.rightContainer}>
+        <div className={styles.avatarContainer} ref={avatarRef}>
+          <Avatar
+            source={configs.pngSource}
+            layers={configs.layers}
+            attributes={attributes}
+          />
+        </div>
+        <div className={styles.btnContainer}>
+          <Button variant="contained" onClick={onShuffle}>
+            Shuffle
+          </Button>
+          <Button variant="contained" onClick={onDownload}>
+            Download
+          </Button>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default PatchPanel;
