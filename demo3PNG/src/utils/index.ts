24afe4c643c38b60d36b9d78b26abb0da3ce6e79
Sean Wang <sean.wang@skyscanner.net>
2022-10-19 17:18:08 +0800

    chore: batchShuffle depend on supply

--- demo3PNG/src/utils/index.ts
+++ demo3PNG/src/utils/index.ts
@@ -4,24 +4,6 @@ import JSZip from "jszip";
 import configs from "../configs";
 import { Attributes } from "../types";
 
-export const getSuppliedAttributes = (total: number) => {
-  return Object.keys(configs.attributes).reduce((accAttrs, attrName) => {
-    const attrStyles = configs.attributes[attrName];
-    const len = attrStyles.length;
-    const perSupply = len >= 0 ? Math.floor(total / len) : 0;
-    const modSupply = len >= 0 ? total % len : 0;
-    return {
-      ...accAttrs,
-      [attrName]: attrStyles.reduce((accStyles, styleName, index) => {
-        return {
-          ...accStyles,
-          [styleName]: index === 0 ? perSupply + modSupply : perSupply,
-        };
-      }, {} as Record<string, number>),
-    };
-  }, {} as Record<string, Record<string, number>>);
-};
-
 export const shuffle = () => {
   return Object.keys(configs.attributes).reduce((accAttrs, attrName) => {
     const collection = configs.attributes[attrName];
@@ -52,6 +34,63 @@ export const download = async (attributes: Attributes, ele: HTMLDivElement) => {
   }
 };
 
-export function random<T>(options: T[]): T | undefined {
+function random<T>(options: T[]): T | undefined {
   return options[Math.floor(Math.random() * options.length)];
 }
+
+/**
+ * @param {number} total e.g. 3
+ * @param {Record<string, Array<string>>} attributes e.g. {skin: ['default', 'white']}
+ * @returns {Record<string, Record<string, number>>} e.g. {skin: {default: 2, white: 1}}
+ */
+export const getAttributesWithSupply = (
+  total: number,
+  attributes: Record<string, Array<string>>
+) => {
+  return Object.keys(attributes).reduce((accAttrs, attrName) => {
+    const attrStyles = attributes[attrName];
+    const len = attrStyles.length;
+    const perSupply = len >= 0 ? Math.floor(total / len) : 0;
+    const modSupply = len >= 0 ? total % len : 0;
+    return {
+      ...accAttrs,
+      [attrName]: attrStyles.reduce((accStyles, styleName, index) => {
+        return {
+          ...accStyles,
+          [styleName]: index === 0 ? perSupply + modSupply : perSupply,
+        };
+      }, {} as Record<string, number>),
+    };
+  }, {} as Record<string, Record<string, number>>);
+};
+
+/**
+ * @param {number} total e.g. 3
+ * @param {Record<string, Array<string>>} attributes e.g. {skin: ['default', 'white']}
+ * @returns {Array<Record<string, string>>} e.g. [{skin: 'default'}, {skin: 'white'}, {skin: 'default'}]
+ */
+export const batchShuffle = (
+  total: number,
+  attributes: Record<string, Array<string>>
+): Array<Record<string, string>> => {
+  const suppliedAttributes = getAttributesWithSupply(total, attributes);
+  const entities = new Array(total).fill(0).map((_, index) => {
+    return Object.keys(suppliedAttributes).reduce((acc, attrName) => {
+      const curAttrStyles = suppliedAttributes[attrName];
+      const options = Object.keys(curAttrStyles).filter(
+        (styleName) => curAttrStyles[styleName]
+      );
+
+      const randomStyle = random(options);
+
+      if (randomStyle === undefined) return acc;
+
+      curAttrStyles[randomStyle] -= 1;
+      return {
+        ...acc,
+        [attrName]: randomStyle,
+      };
+    }, {} as Record<string, string>);
+  });
+  return entities;
+};
