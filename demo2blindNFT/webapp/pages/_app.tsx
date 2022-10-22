188c249dc986580a183454b49ce06d2c3c568126
Linch Liu <linch.liu@skyscanner.net>
2022-10-22 10:13:53 +0800

    update env

--- demo2blindNFT/webapp/pages/_app.tsx
+++ demo2blindNFT/webapp/pages/_app.tsx
@@ -18,13 +18,24 @@ const ALCHEMY_PROJECT_KEY = process.env.ALCHEMY_PROJECT_KEY;
 let client: any;
 
 
-// Connect to different network (goerli/localhost) according to different environment variables.
-if (process.env.PROVIDER_MODE === "goerli") {
+// Connect to different network (production/goerli/localhost) according to different environment variables.
 
-  console.log('goerli')
+if (process.env.PROVIDER_MODE === "production") {
+
+  const { chains, provider, webSocketProvider } = configureChains(
+    defaultChains,
+    [publicProvider()]
+  );
+
+  client = createClient({
+    autoConnect: true,
+    connectors: [new MetaMaskConnector({ chains })],
+    provider,
+    webSocketProvider,
+  });
+} else if (process.env.PROVIDER_MODE === "goerli") {
 
   // Goerli test network
-
   const { chains, provider, webSocketProvider } = configureChains(
     defaultChains,
     [alchemyProvider({ apiKey: ALCHEMY_PROJECT_KEY }), publicProvider()]
@@ -40,9 +51,6 @@ if (process.env.PROVIDER_MODE === "goerli") {
 } else {
 
   // localhost network
-
-  console.log('localhost network')
-
   const { chains, provider, webSocketProvider } = configureChains(
     defaultChains,
     [
@@ -61,8 +69,6 @@ if (process.env.PROVIDER_MODE === "goerli") {
     webSocketProvider,
   });
 }
-
-// TODO: mainnet network
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
