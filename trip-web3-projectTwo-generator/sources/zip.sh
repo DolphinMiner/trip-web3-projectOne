2ccf1c148f6063aa0de3f62a0b2f4a194e6433b5
xiaobo_wang <sean.wang@skyscanner.net>
2023-04-27 16:41:20 +0800

    chore: add zip.sh

---
+++ trip-web3-projectTwo-generator/sources/zip.sh
@@ -0,0 +1,37 @@
+rm -rf ./**/*.zip
+
+cd ./BlkDiamond
+cd ./Background && rm -rf *.zip && zip -r Background.zip ./*.png
+cd ..
+cd ./Pattern && rm -rf *.zip && zip -r Pattern.zip ./*.png
+cd ..
+cd ./Number
+cd ./HundredsPlace && rm -rf *.zip && zip -r HundredsPlace.zip ./*.png
+cd ..
+cd ./TensPlace && rm -rf *.zip && zip -r TensPlace.zip ./*.png
+cd ..
+cd ./OnesPlace && rm -rf *.zip && zip -r OnesPlace.zip ./*.png
+
+cd ../../../Gold
+cd ./Background && rm -rf *.zip && zip -r Background.zip ./*.png
+cd ..
+cd ./Pattern && rm -rf *.zip && zip -r Pattern.zip ./*.png
+cd ..
+cd ./Number
+cd ./HundredsPlace && rm -rf *.zip && zip -r HundredsPlace.zip ./*.png
+cd ..
+cd ./TensPlace && rm -rf *.zip && zip -r TensPlace.zip ./*.png
+cd ..
+cd ./OnesPlace && rm -rf *.zip && zip -r OnesPlace.zip ./*.png
+
+cd ../../../Platinum
+cd ./Background && rm -rf *.zip && zip -r Background.zip ./*.png
+cd ..
+cd ./Pattern && rm -rf *.zip && zip -r Pattern.zip ./*.png
+cd ..
+cd ./Number
+cd ./HundredsPlace && rm -rf *.zip && zip -r HundredsPlace.zip ./*.png
+cd ..
+cd ./TensPlace && rm -rf *.zip && zip -r TensPlace.zip ./*.png
+cd ..
+cd ./OnesPlace && rm -rf *.zip && zip -r OnesPlace.zip ./*.png
